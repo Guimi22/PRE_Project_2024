b9a9edc111b267194823f160da32cb6e03061a43
Guillem <136318706+guimi22@users.noreply.github.com>
2024-04-02 15:55:45 +0200

    aversiara

---
+++ README.md
@@ -0,0 +1,1 @@
+# PRE_Project_2024
---
+++ proyectoPRE/robot.py
@@ -0,0 +1,11 @@
+class robot:
+    def __init__(self, peso, tamaño, posicion, carga):
+        self.pos = posicion
+        self.tam = tamaño
+        self.pes = peso
+        self.car = carga
+    def recoger(self, item):
+        self.car = item.sn
+    def dejar(self, item):
+        self.car = 0;
+
