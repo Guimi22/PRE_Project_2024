5821435f484f8c0ebd0cc3da8be510735e3c0c38
Guimi22 <guillemroper@gmail.com>
2024-06-25 16:14:02 +0200

    main funcional

--- proyectoPRE/almacen.py
+++ proyectoPRE/almacen.py
@@ -3,7 +3,8 @@ import pygame
 from pygame.locals import *
 
 from items import *
-
+import heapq
+import math
 import os
 
 pygame.init()
@@ -16,8 +17,6 @@ class Almacen:
 
     def funct_read_excel(self, filename):
         with open(filename, mode='r') as f:
-            lines_aux1 = []
-            lines_aux2 = []
             list_excel = []
             for line in f:
                 lines_aux1 = line.split(';')
@@ -30,50 +29,118 @@ class Almacen:
                 list_excel.append(lines_aux1)
         return list_excel
 
+    # def arange_order(self, filename):
+    #     order = self.funct_read_excel(filename)
+    #     order_new = []
+    #     order_new_snqty = []
+    #
+    #     for item in order:
+    #         #item_in_almacen = self.find_item(item[0])
+    #         posx_aux = self.items[item[0]][2]
+    #         posy_aux = self.items[item[0]][3]
+    #         sn_item = item[0]
+    #         qty_item = item[1]
+    #         lista_aux1 = [posx_aux, posy_aux]
+    #         lista_aux2 = [sn_item, qty_item]
+    #         long = len(order_new)
+    #         if long == 0:
+    #             order_new = [lista_aux1]
+    #             order_new_snqty = [lista_aux2]
+    #         else:
+    #             i = 0
+    #             for element in order_new:
+    #                 i += 1
+    #                 closer_item_x = element[0]
+    #                 closer_item_y = element[1]
+    #                 if posx_aux < closer_item_x:
+    #                     order_new.insert(i-1, lista_aux1)
+    #                     order_new_snqty.insert(i-1, lista_aux2)
+    #                     break
+    #                 elif posx_aux == closer_item_x:
+    #                     if posy_aux < closer_item_y:
+    #                         order_new.insert(i-1, lista_aux1)
+    #                         order_new_snqty.insert(i-1, lista_aux2)
+    #                         break
+    #                     else:
+    #                         order_new.insert(i, lista_aux1)
+    #                         order_new_snqty.insert(i, lista_aux2)
+    #                         break
+    #                 else:
+    #                     if i == long:
+    #                         order_new.append(lista_aux1)
+    #                         order_new_snqty.append(lista_aux2)
+    #                         break
+    #                     else:
+    #                         break
+    #     return order_new, order_new_snqty
+
+    def distance(self, pos1, pos2):
+        return math.sqrt((pos1[0] - pos2[0]) ** 2 + (pos1[1] - pos2[1]) ** 2)
+
+    # def arange_order(self, filename):
+    #     order = self.funct_read_excel(filename)
+    #     item_positions = {item[0]: (self.items[item[0]][2], self.items[item[0]][3]) for item in order}
+    #     item_weights = {item[0]: self.items[item[0]][1] for item in order}
+    #
+    #     # Assume the starting point is the origin (0,0)
+    #     start_position = (0, 0)
+    #     order_list = []
+    #
+    #     # Priority queue to store (cost, current_position, path_taken)
+    #     pq = [(0, start_position, [])]
+    #     visited = set()
+    #
+    #     while pq:
+    #         current_cost, current_position, path = heapq.heappop(pq)
+    #
+    #         if current_position in visited:
+    #             continue
+    #         visited.add(current_position)
+    #
+    #         if current_position in item_positions.values():
+    #             item = next(k for k, v in item_positions.items() if v == current_position)
+    #             order_list.append((item, item_weights[item]))
+    #             path.append(item)
+    #             del item_positions[item]
+    #             if not item_positions:
+    #                 break
+    #
+    #         for next_item, next_position in item_positions.items():
+    #             if next_position not in visited:
+    #                 distance = self.distance(current_position, next_position)
+    #                 weight = float(item_weights[next_item])
+    #                 heapq.heappush(pq, (current_cost + distance + weight, next_position, path[:]))
+    #     return order_list
+
     def arange_order(self, filename):
         order = self.funct_read_excel(filename)
-        order_new = []
-        order_new_snqty = []
-
+        item_positions = {}
         for item in order:
-            #item_in_almacen = self.find_item(item[0])
-            posx_aux = self.items[item[0]][2]
-            posy_aux = self.items[item[0]][3]
-            sn_item = item[0]
-            qty_item = item[1]
-            lista_aux1 = [posx_aux, posy_aux]
-            lista_aux2 = [sn_item, qty_item]
-            long = len(order_new)
-            if long == 0:
-                order_new = [lista_aux1]
-                order_new_snqty = [lista_aux2]
-            else:
-                i = 0
-                for element in order_new:
-                    i += 1
-                    closer_item_x = element[0]
-                    closer_item_y = element[1]
-                    if posx_aux < closer_item_x:
-                        order_new.insert(i-1, lista_aux1)
-                        order_new_snqty.insert(i-1, lista_aux2)
-                        break
-                    elif posx_aux == closer_item_x:
-                        if posy_aux < closer_item_y:
-                            order_new.insert(i-1, lista_aux1)
-                            order_new_snqty.insert(i-1, lista_aux2)
-                            break
-                        else:
-                            order_new.insert(i, lista_aux1)
-                            order_new_snqty.insert(i, lista_aux2)
-                            break
-                    else:
-                        if i == long:
-                            order_new.append(lista_aux1)
-                            order_new_snqty.append(lista_aux2)
-                            break
-                        else:
-                            break
-        return order_new, order_new_snqty
+            item_positions[item[0]] = [self.items[item[0]][2], self.items[item[0]][3]], self.items[item[0]][1], [item[0], item[1]]
+        # item_weights = {item[0]: self.items[item[0]][1] for item in order}
+        position = [50, 440]
+        # funcion que devuelva posicion actual del robot en el almacen
+        iter_finish = 1
+        weighted_distances = []
+        order_route = []
+        order_snqty = []
+        snqty_lista = []
+
+        while iter_finish:
+            for item in item_positions:
+                weighted_distances.append(item_positions[item][1] * self.distance(position, item_positions[item][0]))
+                snqty_lista.append(item_positions[item][2])
+            next_node = min(weighted_distances)
+            indice_lista = weighted_distances.index(next_node)
+            order_route.append(item_positions[snqty_lista[indice_lista][0]][0])
+            order_snqty.append(snqty_lista[indice_lista])
+            position = order_route[len(order_route) - 1]
+            del (item_positions[snqty_lista[indice_lista][0]])
+            weighted_distances = []
+            snqty_lista = []
+            if len(item_positions) == 0:
+                iter_finish = 0
+        return order_route, order_snqty
 
     def update_stock(self, filename):
         list_stock = self.funct_read_excel(filename)
--- proyectoPRE/main.py
+++ proyectoPRE/main.py
@@ -10,38 +10,58 @@ blanco = (255, 255, 255)
 i = 0
 def main():
 
-    # pygame.init()
-    # p = MainWindow(ancho_pantalla,alto_pantalla,3,3,3,3,3)
+    pygame.init()
+    p = MainWindow(ancho_pantalla,alto_pantalla,3,3,3,3,3)
     diccionario = {}
     A = Almacen(3,4,diccionario)
     prova = A.update_stock('Libro1.csv')
-    #prova1 = A.find_item('55572f')
-    prova2 = A.items
-    print(prova2)
-    #print(prova2['5433fg'][2])
-    prova3 = A.funct_read_excel('Libro2.csv')
-    print(prova3)
-    comanda, comanda_snqty = A.arange_order('Libro2.csv')
-    print(comanda, comanda_snqty)
-    # i = 0
-    # for item in comanda:
-    #     i += 1
-    #     MainWindow.ruta(p, item[0], item[1])
-    #     MainWindow.recoger(p, i)
-    #     A.update_items(comanda_snqty[i-1][0], comanda_snqty[i-1][1])
-    #     print(A.items)
-    # i = 0
-    # MainWindow.puerta(p)
-    # MainWindow.dejar(p)
-    # MainWindow.home(p)
+    # #prova1 = A.find_item('55572f')
+    # prova2 = A.items
+    # print(prova2)
+    # #print(prova2['5433fg'][2])
+    # prova3 = A.funct_read_excel('Libro2.csv')
+    # print(prova3)
+    comanda,comanda_snqty = A.arange_order('Libro2.csv')
+    # print(comanda, comandasnqty)
+
+    # order = A.funct_read_excel('Libro2.csv')
+    # item_positions = {}
+    # for item in order:
+    #     item_positions[item[0]] = [A.items[item[0]][2], A.items[item[0]][3]], A.items[item[0]][1], [item[0], item[1]]
+    # # item_weights = {item[0]: self.items[item[0]][1] for item in order}
+    # position = [50, 440]
+    # # funcion que devuelva posicion actual del robot en el almacen
+    # iter_finish = 1
+    # weighted_distances = []
+    # order_route = []
+    # order_snqty = []
+    # snqty_lista = []
     #
-    #
-    # for event in pygame.event.get():
-    #     if event.type == pygame.QUIT:
-    #         sys.exit()
-    #
-    #
-    #     pygame.display.update()
+    # for item in item_positions:
+    #     weighted_distances.append(item_positions[item][1] * A.distance(position, item_positions[item][0]))
+    #     snqty_lista.append(item_positions[item][2])
+    # next_node = min(weighted_distances)
+    # indice_lista = weighted_distances.index(next_node)
+    # order_route.append(item_positions[snqty_lista[indice_lista][0]][0])
+    # print(order_route, item_positions, snqty_lista, indice_lista)
+    i = 0
+    for item in comanda:
+        i += 1
+        MainWindow.ruta(p, item[0], item[1])
+        MainWindow.recoger(p, i)
+        A.update_items(comanda_snqty[i-1][0], comanda_snqty[i-1][1])
+        print(A.items)
+    MainWindow.puerta(p)
+    MainWindow.dejar(p)
+    MainWindow.home(p)
+
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+
+        pygame.display.update()
 
 
 if __name__ == "__main__":
